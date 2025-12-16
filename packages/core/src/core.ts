e2f4d81dfd0c2a64de23d50fd3524af7ae1e8f95
Aniket Bansal <aniket.bansal@iongroup.com>
2025-12-16 20:57:46 +0530

    - Added ɵɵStandaloneFeature

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -146,5 +146,6 @@ if (typeof ngDevMode !== 'undefined' && ngDevMode) {
 // Compat layer
 export {InputFlags as ɵɵInputFlags} from './render3/interfaces/input_flags';
 export {ɵɵInputTransformsFeature} from './compat/input_transforms_feature';
+export {ɵɵStandaloneFeature} from './compat/standalone_feature';
 export {ɵɵpropertyInterpolate} from './compat/property_interpolate';
 export {ɵɵclassMapInterpolate1} from './compat/class_map_interpolate';
